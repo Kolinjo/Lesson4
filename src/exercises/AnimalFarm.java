bf71a4d390e2d5ebf8a7cee815c0e81b5be758ef
Kolinjo <kozicmarko@yahoo.com>
2019-05-13 00:14:51 +0200

    AnimalFarmChange

--- src/exercises/AnimalFarm.java
+++ src/exercises/AnimalFarm.java
@@ -9,11 +9,35 @@ import javax.swing.JOptionPane;
 
 public class AnimalFarm {
 
+	
 	AnimalFarm() {
 		/* 1. Ask the user which animal they want, then play the sound of that animal. */
 
 		/* 2. Make it so that the user can keep entering new animals. */
-
+			
+		for (int i = 0; i < 6; i++) {
+		String question = JOptionPane.showInputDialog("Which animal you want to play sound?? cow,duck,dog,cat,lama,pig");
+		
+		if (question.equals("cow")) {
+			playMoo();
+		}
+		else if (question.equals("duck")) {
+			playQuack();
+		}
+		else if (question.equals("dog")) {
+			playWoof();
+		}
+		else if (question.equals("meow")) {
+			playMeow();
+		}
+		else if (question.equals("lama")) {
+			playLama();
+		}
+		else if (question.equals("oink")) {
+			playOink();
+		}
+	
+		}
 	}
 
 	void playMoo() {
@@ -27,12 +51,25 @@ public class AnimalFarm {
 	void playWoof() {
 		playNoise(woofFile);
 	}
+	
+	void playMeow () {
+		playNoise(meowFile);
+	}
+	
+	void playLama () {
+		playNoise(llamaFile);
+	}
+	
+	void playOink () {
+		playNoise(oinkFile);
+	}
 
 	String quackFile = "sound/quack.wav";
 	String mooFile = "sound/moo.wav";
 	String woofFile = "sound/woof.wav";
 	String meowFile = "sound/meow.wav";
 	String llamaFile = "sound/llama.wav";
+	String oinkFile = "sound/oink.wav";
 
 	/* Ignore this stuff */
 
